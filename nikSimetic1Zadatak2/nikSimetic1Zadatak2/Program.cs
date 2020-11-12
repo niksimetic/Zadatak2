176ced872279e753c74a745e442873de37ae0560
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-11-12 10:30:58 +0100

    Add files via upload

---
+++ nikSimetic1Zadatak2/nikSimetic1Zadatak2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimetic1Zadatak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Upisite znakove");
+            string znakovi = Console.ReadLine();
+
+            Console.WriteLine(znakovi.Replace(" ", "_"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
